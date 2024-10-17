c3345e5a068402473ee431b0659e7e8f0ba00fab
harmannugen <harmannugen@gmail.com>
2024-10-17 12:53:21 +0530

    fix about us design

--- components/homepage/AboutSection.tsx
+++ components/homepage/AboutSection.tsx
@@ -1,96 +1,142 @@
-'use client'
-import React from "react";
-import Marquee from "../ui/Marquee";
+"use client";
+import React, { useLayoutEffect, useRef } from "react";
 import Image from "next/image";
-interface aboutContentType {
-    header_title: string,
-    header_description: string,
-    animated_title: string,
-    button_value: string,
-    "img-1": string,
-    "img-2": string
+import { gsap } from "gsap";
+import MarqueStarIcon from "../icons/StarIcon";
+
+interface AboutContentType {
+  header_title: string;
+  header_description: string;
+  animated_title: string;
+  button_value: string;
+  "img-1": string;
+  "img-2": string;
 }
 interface DataProps {
-    header_title: string,
-    header_description: string,
-    animated_title: string,
-    button_value: string,
-    post_title: string,
-    post_image: string,
-    post_description: string,
+  header_title: string;
+  header_description: string;
+  animated_title: string;
+  button_value: string;
+  post_title: string;
+  post_image: string;
+  post_description: string;
 }
 interface ServiceClientsProps {
-    className?: string;
-    pageContent: aboutContentType
-    Data1?: DataProps[]
-    Data2?: DataProps[]
+  className?: string;
+  pageContent: AboutContentType;
+  Data1?: DataProps[];
+  Data2?: DataProps[];
 }
-const AboutSection: React.FC<ServiceClientsProps> = ({ className, pageContent, Data1, Data2 }) => {
-    return (
-        <>
-            <div className="">
-                <div className="relative">
-                    <div className="py-4">
-                        <div className="w-full lg:absolute lg:bottom-0 lg:left-[7%] lg:-rotate-90 origin-bottom-left">
-                            <Marquee title={pageContent?.animated_title} />
-                        </div>
-                    </div>
-                    <div className="p-4 pt-16 flex flex-col lg:flex-row  justify-center items-center">
-                        <div className=" flex lg:flex-col  w-full ">
-                            <div className="">
-                            <div className="w-full lg:pl-24   ">
-                                <div className="pt-8 w-[90%]">Welcome to PBX Technologies Inc., your trusted partner for innovative telecom and IT solutions across North America. With over a decade of expertise in the industry, we’ve built a reputation for excellence, delivering cutting-edge services that keep businesses connected, efficient, and ahead of the curve.</div>
-                            </div>
-                                {Data1?.map((value) => {
-                                    return (
-                                        <>
-                                            <div className="w-full lg:pl-24   ">
-                                                <div className="text-primary text-2xl pt-4">
-                                                    {value?.post_title}</div>
-                                                <div className="pt-4 w-[90%]">{value?.post_description}</div>
-                                            </div>
-                                        </>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                        <div className="h-full w-[45%] flex justify-start items-center p-2 ">
-                            <Image src={pageContent?.["img-1"]} alt="image" height={500} width={500} className="object-scale-down" />
-                        </div>
-                    </div>
 
-                </div>
+const AboutSection: React.FC<ServiceClientsProps> = ({
+  className,
+  pageContent,
+  Data1,
+  Data2,
+}) => {
+  const titleRef = useRef(null);
 
-                <div className="relative">
-                    <div className="py-4 invisible lg:visible">
-                        <div className="w-[600px] lg:absolute lg:top-0 lg:right-[7%] lg:-rotate-90 origin-top-right">
-                            <Marquee title={"About Us"} />
-                        </div>
-                    </div>
-                    <div className="p-4 pt-16 flex flex-col lg:flex-row justify-center items-center">
-                        <div className="h-full w-[45%] flex justify-start items-center p-2">
-                            <Image src={pageContent?.["img-2"]} alt="image" height={500} width={500} className="object-scale-down" />
-                        </div>
-                        <div className=" lg:flex-col w-full">
-                            {Data2?.map((value) => {
-                                return (
-                                    <>
-                                        <div className="w-full lg:pl-24   ">
-                                            <div className="text-primary text-2xl">
-                                                {value?.post_title}</div>
-                                            <div className="pt-8 w-[90%]">{value?.post_description}</div>
-                                        </div>
-                                    </>
-                                )
-                            })}
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
-        </>
+  useLayoutEffect(() => {
+    gsap.fromTo(
+      titleRef.current,
+      {
+        x: "40%",
+      },
+      {
+        x: "0%",
+        stagger: 0.2,
+        duration: 1.5,
+        ease: "easeInOut",
+      }
     );
+  }, []);
+
+  return (
+    <>
+      <div className="relative">
+        <div className="py-4">
+          <div
+            ref={titleRef}
+            className="w-full text-6xl uppercase font-serif flex justify-center font-bold gap-10"
+          >
+            <p className="text-primary">{pageContent?.animated_title}</p>
+            <MarqueStarIcon />
+          </div>
+        </div>
+        <div className="p-4 pt-16 flex flex-col lg:flex-row justify-center items-center">
+          <div className="flex lg:flex-col w-full">
+            <div>
+              <div className="w-full lg:pl-24"></div>
+              <div className="w-full lg:pl-24">
+                <div className="text-primary text-2xl">
+                  {pageContent?.header_title}
+                </div>
+                <div className="pt-8 w-[90%]">
+                  {pageContent?.header_description}
+                </div>
+              </div>
+              {Data1?.map((value) => {
+                return (
+                  <div key={value?.post_title} className="w-full lg:pl-24">
+                    <div className="text-primary text-2xl pt-4">
+                      {value?.post_title}
+                    </div>
+                    <div className="pt-4 w-[90%]">
+                      {value?.post_description}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="h-full w-[45%] flex justify-start items-center p-2">
+            <Image
+              src={pageContent?.["img-1"]}
+              alt="image"
+              height={500}
+              width={500}
+              className="object-scale-down"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="relative">
+        <div className="p-4 pt-16 flex flex-col lg:flex-row justify-center items-center">
+          <div className="hidden lg:flex h-full w-[45%] justify-start items-center p-2">
+            <Image
+              src={pageContent?.["img-2"]}
+              alt="image"
+              height={500}
+              width={500}
+              className="object-scale-down"
+            />
+          </div>
+          <div className="lg:flex-col w-full">
+            {Data2?.map((value) => {
+              return (
+                <div key={value?.post_title} className="w-full lg:pl-24">
+                  <div className="text-primary text-2xl">
+                    {value?.post_title}
+                  </div>
+                  <div className="pt-8 w-[90%]">{value?.post_description}</div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="lg:hidden h-full w-[45%] justify-start items-center p-2">
+            <Image
+              src={pageContent?.["img-2"]}
+              alt="image"
+              height={500}
+              width={500}
+              className="object-scale-down"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutSection;
--- components/homepage/ServiceSection.tsx
+++ components/homepage/ServiceSection.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useLayoutEffect } from "react";
 import { Button } from "../ui";
 import gsap from "gsap";
@@ -6,19 +6,22 @@ import Marquee from "../ui/Marquee";
 import Image from "next/image";
 import MacGirl from "../../image/macgirl.png";
 import Contact from "../../image/contact.png";
-interface ServiceContentType{
-  header_title: string,
-  header_description: string,
-  animated_title: string,
-  button_value: string,
-  hr_email: string
-  image:string
+interface ServiceContentType {
+  header_title: string;
+  header_description: string;
+  animated_title: string;
+  button_value: string;
+  hr_email: string;
+  image: string;
 }
 interface ServiceClientsProps {
   className?: string;
-  pageContent:ServiceContentType
+  pageContent: ServiceContentType;
 }
-const InterestedSection: React.FC<ServiceClientsProps> = ({ className,pageContent }) => {
+const InterestedSection: React.FC<ServiceClientsProps> = ({
+  className,
+  pageContent,
+}) => {
   const handleClick = () => {
     const emailAddress = "hr@nugeninfo.com";
     const emailSubject = "Interested to Work with us";
@@ -43,14 +46,15 @@ const InterestedSection: React.FC<ServiceClientsProps> = ({ className,pageConten
         id="section-Interested-ref"
         className=" w-screen bg-[#080c3c] h-fit overflow-auto lg:p-8 relative top-0 left-0 z-10 lg:shadow-2xl"
       >
-        <div
-          className={` relative items-start w-full h-full ${className}`}
-        >
+        <div className={` relative items-start w-full h-full ${className}`}>
           <div className="w-screen p-8 flex flex-col md:flex-row">
             <div className="w-full">
               <div className="font-serif text-[#ff366b] text-3xl lg:text-5xl">
-                {pageContent?.header_title}</div>
-              <div className="pt-8 w-[60%] text-white">{pageContent?.header_description}</div>
+                {pageContent?.header_title}
+              </div>
+              <div className="pt-8 w-[60%] text-white">
+                {pageContent?.header_description}
+              </div>
               <div className="flex justify-start" id="">
                 <Button
                   onClick={handleClick}
@@ -59,9 +63,16 @@ const InterestedSection: React.FC<ServiceClientsProps> = ({ className,pageConten
                 >
                   {pageContent?.hr_email}
                 </Button>
-              </div></div>
-            <div className="h-full w-full flex justify-start md:-mt-8 md:-ml-32 md:pr-32 items-center p-2">
-              <Image src={pageContent?.image} alt="image" height={500} width={500} className="object-scale-down"/>
+              </div>
+            </div>
+            <div className="h-full w-full flex justify-start md:-mt-8 md:-ml-32 items-center p-2">
+              <Image
+                src={pageContent?.image}
+                alt="image"
+                height={500}
+                width={500}
+                className="object-scale-down"
+              />
             </div>
           </div>
         </div>
