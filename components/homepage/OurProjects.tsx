098c86528673992e9e0e268433986921db06484d
Pardeep24k <pardeep1209k@gmail.com>
2024-10-25 18:04:17 +0530

    fix design project page

--- components/homepage/OurProjects.tsx
+++ components/homepage/OurProjects.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { blurAnimation, leftToRightAnimation, rightToLeftAnimation, staggerParent, topToBottomAnimation } from "@/lib/animation/animationUtils";
+import { blurAnimation, leftToRightAnimation, staggerParent, topToBottomAnimation } from "@/lib/animation/animationUtils";
+
 interface OurProjectsType {
   buttonData: any;
 }
@@ -11,32 +11,32 @@ interface OurProjectsType {
 const OurProjects: React.FC<OurProjectsType> = ({ buttonData }) => {
   return (
     <motion.section
-    {...staggerParent} 
+      {...staggerParent}
       id="featured-work-section"
-      className="w-screen lg:h-screen h-screen section-featured-ref overflow-hidden lg:p-8 relative lg:fixed top-0 left-0 z-10 bg-background shadow-2xl"
+      className="w-screen lg:h-screen h-auto section-featured-ref overflow-hidden lg:p-8 relative bg-background shadow-2xl  lg:fixed top-0 left-0 z-10"
     >
-      <div className="flex flex-col justify-center lg:grid relative items-start w-full lg:h-screen h-full">
-        <motion.div variants={topToBottomAnimation} className="w-full text-center text-4xl md:text-6xl capitalize font-serif font-bold text-primary">
+      <div className="flex flex-col justify-center items-center lg:grid  relative w-full h-full">
+        <motion.div variants={topToBottomAnimation} className="w-full text-center text-4xl md:text-6xl capitalize font-serif font-bold text-primary mb-4">
           Our Projects
         </motion.div>
-        <div className="flex flex-col justify-center items-center lg:-mt-12 ">
+        <div className="flex flex-col justify-center items-center">
           {buttonData?.length >= 2 && (
-            <div className="gap-12 justify-center items-center flex flex-col lg:flex-row ">
+            <div className="gap-8 flex flex-col lg:flex-row justify-center items-center">
               {buttonData?.map((value: any, index: number) => (
                 <motion.div
-                variants={blurAnimation}
+                  variants={blurAnimation}
                   key={value.button_title + index}
-                  className="lg:h-[350px] bg-white md:w-full w-full lg:w-[400px] shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out min-w-[280px] text-black rounded-[22px] border-none md:mx-2 my-6 relative lg:flex lg:flex-col lg:items-center lg:justify-center card group"
+                  className="lg:h-[350px] bg-white w-full lg:w-[350px] shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out min-w-[280px] text-black rounded-[22px] border-none my-4 relative lg:flex lg:flex-col lg:items-center lg:justify-center card group"
                 >
-                  <div className="relative flex flex-col items-center p-6">
-                    <motion.h2 variants={leftToRightAnimation} className="font-extrabold text-2xl text-center group-hover:text-[#ff366b] transition-colors duration-300 capitalize font-serif text-primary">
+                  <div className="relative flex flex-col items-center p-4">
+                    <motion.h2 variants={leftToRightAnimation} className="font-extrabold text-xl text-center group-hover:text-[#ff366b] transition-colors duration-300 capitalize font-serif text-primary">
                       {value?.button_title || ""}
                     </motion.h2>
-                    <motion.div variants={leftToRightAnimation} className="justify-center mb-16">
+                    <motion.div variants={leftToRightAnimation} className="justify-center mb-4">
                       <img
                         src={value?.button_image}
                         alt={value?.button_title}
-                        className="w-[150px] lg:w-[200px] transition-transform duration-500 transform group-hover:scale-110"
+                        className="w-[120px] lg:w-[180px] transition-transform duration-500 transform group-hover:scale-110"
                       />
                     </motion.div>
                   </div>
